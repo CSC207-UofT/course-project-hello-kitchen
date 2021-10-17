9622d751660c63198abd1f3464b8cf60366c9495
Jiantong Lyu <sandsievedevelop@gmail.com>
2021-10-16 22:41:56 -0400

    Update User

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,5 +1,15 @@
 package user;
 
 public abstract class User{
+    String name;
+    String description;
 
+    public User(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    public abstract String getName();
+
+    public abstract String toString();
 }

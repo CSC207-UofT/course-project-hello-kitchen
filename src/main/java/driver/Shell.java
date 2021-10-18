0355b9ea13f7aabde8cef96f2384a938792279e8
Tianle Wang <tianle.wang@mail.utoronto.ca>
2021-10-18 12:06:12 -0400

    update Shell.java

--- src/main/java/driver/Shell.java
+++ src/main/java/driver/Shell.java
@@ -5,19 +5,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-/**
- * This class holds all the states of the Shell Program
- */
 public class Shell{
 
     private boolean running;
-
-
-    /**
-     * Constructs an object that holds JShell's important states including
-     * the root directory, current directory, directory stack and a boolean that
-     * indicates JShell is running
-     */
     public Shell() {
         this.running = true;
 

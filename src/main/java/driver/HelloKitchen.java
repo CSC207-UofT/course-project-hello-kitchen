59c001038bff8d077daf973b1da7b95f80433a1f
sandsieve <sandsievel@gmail.com>
2021-12-04 22:14:37 -0500

    Update shell and hello kitchen

--- src/main/java/driver/HelloKitchen.java
+++ src/main/java/driver/HelloKitchen.java
@@ -2,11 +2,31 @@ package driver;
 
 
 
+import commands.Command;
+import commands.CommandFactory;
+import commands.Token;
+
 import java.io.IOException;
 import java.util.Scanner;
 
 
 public class HelloKitchen {
 
+    public static void run(String commandLine){
+        Token token = new Token(commandLine);
+        CommandFactory factory = new CommandFactory(token);
+        Command command = factory.getCommand();
+        command.run(commandLine);
+    }
+
+    public static void main(String[] args) {
+        Shell shell = new Shell();
+        Scanner in = new Scanner(System.in);
+        while (shell.isRunning()) {
+            System.out.print("$ ");
+            String commandLine = in.nextLine();
+            run(commandLine);
+        }
+    }
 
 }

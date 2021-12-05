15319a048d890e269fad7cc76ee3f6419ea535b0
Jessica Hu <jessica.hu@mail.utoronto.ca>
2021-12-04 23:20:47 -0500

    Fix gradle issues

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+        ObjectMapper mapper = new ObjectMapper();
     }
 }

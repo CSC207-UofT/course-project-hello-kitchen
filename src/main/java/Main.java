9231826111619f83e3250e8c52c85cc4f0fa6b06
sandsieve <sandsievel@gmail.com>
2021-12-07 15:32:20 -0500

    Add disk read/write to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import commands.Command;
 import commands.CommandFactory;
+import diskoperator.DiskOperator;
+import manager.RecipeManager;
+import manager.UserManager;
+import module.InstanceRegistry;
 import module.Token;
 import module.Shell;
 
@@ -11,6 +15,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Hello");
         ObjectMapper mapper = new ObjectMapper();
+        DiskOperator diskOperator = new DiskOperator();
+        try {
+            UserManager userManager = (UserManager) diskOperator.
+                    read("userManager.json", InstanceRegistry.getUserManager());
+            RecipeManager recipeManager = (RecipeManager) diskOperator.
+                    read("recipeManager.json", InstanceRegistry.getRecipeManager());
+        }
+        catch (IOException e) {
+            System.out.println("No data found, starting with empty dataset.");
+        }
+        UserManager userManager = InstanceRegistry.getUserManager();
+        RecipeManager recipeManager = InstanceRegistry.getRecipeManager();
         Shell shell = new Shell();
         Scanner in = new Scanner(System.in);
         while (shell.isRunning()) {
@@ -18,6 +34,9 @@ public class Main {
             String commandLine = in.nextLine();
             run(commandLine);
         }
+        System.out.println("Bye");
+        diskOperator.save("userManager.json", userManager);
+        diskOperator.save("recipeManager.json", recipeManager);
     }
 
     public static void run(String commandLine){
